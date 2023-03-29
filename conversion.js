df1674304c7206554893e76bb2cb8e1d95e92ac9
vishnukhanna <117109719+vishnukhanna@users.noreply.github.com>
2023-03-29 15:46:13 +0530

    Add files via upload
    Temperature converter

---
+++ conversion.js
@@ -0,0 +1,8 @@
+
+var c = [];	
+for (fahrenheit = 20; fahrenheit <= 120; fahrenheit += 5){
+	celsius = (fahrenheit - 32) * (5 / 9);
+	celsius = Math.round(celsius * 100) / 100;
+	c[fahrenheit] = celsius;
+
+}
